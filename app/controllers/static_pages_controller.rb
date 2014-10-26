f386ab59ce9ff309baba687a8ab876b5fe542e4a
Steven Tang <stang12290@gmail.com>
2014-10-26 02:11:10 -0700

    fix bug that prevents feed from rendering for signed out users

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,12 +2,16 @@ class StaticPagesController < ApplicationController
   def home
   	if signed_in?
   		@feed_items = current_user.feed.paginate(:page => params[:page], :per_page => 20)
-#  	else
-#  		@feed_items = default_feed
+	 	else
+  		@feed_items = default_feed.paginate(:page => params[:page], :per_page => 20)
   	end
   end
 
   def about
   end
-  
+
+  private
+	  def default_feed
+	  	Post.all
+	  end  
 end
